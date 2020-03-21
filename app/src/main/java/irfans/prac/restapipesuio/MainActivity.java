7df8500db9ce8161c8939a7bdb8dbd161b4896a8
Irfan <irfansa1@ymail.com>
2020-03-21 11:41:52 +0530

    - Updated and tested code

--- app/src/main/java/irfans/prac/restapipesuio/MainActivity.java
+++ app/src/main/java/irfans/prac/restapipesuio/MainActivity.java
@@ -1,17 +1,17 @@
 package irfans.prac.restapipesuio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -19,7 +19,6 @@ import javax.net.ssl.HttpsURLConnection;
 public class MainActivity extends AppCompatActivity {
 
     String URL_str = "https://pesuiorestful.herokuapp.com/";
-    URL url;
     HttpsURLConnection myConn;
     InputStream inputStream;
     TextView status;
@@ -29,59 +28,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        status = findViewById(R.id.status_view);
         increments = findViewById(R.id.count_view);
-        try {
-            url = new URL(URL_str);
-            myConn =
-                    (HttpsURLConnection) url.openConnection();
-            if(myConn.getResponseCode()==200){
-                // do nothing
-            }else{
-                Toast.makeText(this,"Connection failed",Toast.LENGTH_LONG).show();
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
     public void incrCounter( View v) throws IOException {
-        URL_str = URL_str+"/increment";
-        url = new URL(URL_str);
-
-        String name = "my"+"name";
-        myConn =
-                (HttpsURLConnection) url.openConnection();
-        inputStream = myConn.getInputStream();
-        InputStreamReader is = new InputStreamReader(inputStream,"UTF-8");
-        BufferedReader bufferedReader = new BufferedReader(is);
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            stringBuilder.append(line).append("\n");
-        }
-        bufferedReader.close();
-        status.setText(line);
+        Downloader task = new Downloader();
+        task.execute(URL_str+"/increment");
 
     }
 
     public void viewCounter(View v) throws IOException {
-        URL_str = URL_str+"/view";
-        url = new URL(URL_str);
-        myConn =
-                (HttpsURLConnection) url.openConnection();
-        inputStream = myConn.getInputStream();
-        InputStreamReader is = new InputStreamReader(inputStream,"UTF-8");
-        BufferedReader bufferedReader = new BufferedReader(is);
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            stringBuilder.append(line).append("\n");
+        Downloader task = new Downloader();
+        task.execute(URL_str+"/view");
+    }
+    public class Downloader extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            //Log.d(TAG, "Entered doInBackground for Dowlaoder");
+            try
+            {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                //Log.d(TAG, "Fetching Downloader URL:" + url);
+
+                while(data!=-1)
+                {
+                    char current = (char)data;
+                    result = result + current;
+                    data = reader.read();
+                }
+                Log.d("irf", "result is " + result);
+                return result;
+            }
+            catch(Exception e)
+            {
+                //dialogHandler.hideDialog();
+
+                e.printStackTrace();
+                Log.d("irf", "Error met"+e);
+            }
+
+            return null;
         }
-        bufferedReader.close();
-        increments.setText(line);
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            increments.setText(s);
+            //dialogHandler.hideDialog();
+            Log.d("irf", "Execution successful");
+        }
     }
 }
